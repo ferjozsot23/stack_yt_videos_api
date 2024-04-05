34eb05c54305063871bc0c69d75700b8e6b16c21
Fernando Soto <fernando.soto@epn.edu.ec>
2024-04-05 10:12:12 -0500

    feat: using process.env to api_key

---
+++ src/api/client.js
@@ -0,0 +1,16 @@
+import { google } from "googleapis"
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const api_key_secret = process.env.YOUTUBE_API_KEY;
+
+const api_version = "v3"
+const api_key = api_key_secret
+
+const client = google.youtube({
+    version: api_version,
+    auth: api_key,
+})
+
+export default client
