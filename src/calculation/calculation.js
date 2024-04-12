23cf375113c26067de587269f164136ba5787dad
Fernando Soto <fernando.soto@epn.edu.ec>
2024-04-12 15:14:23 -0500

    feat: add validations

--- src/calculation/calculation.js
+++ src/calculation/calculation.js
@@ -1,22 +1,32 @@
 const getTopLikedVideos = (data) =>
-    data
-        .sort((videoA, videoB) => videoB.likeCount - videoA.likeCount)
-        .slice(0, 5)
+    Array.isArray(data) && data
+        ? data
+              .filter((video) => video)
+              .sort((videoA, videoB) => videoB.likeCount - videoA.likeCount)
+              .slice(0, 5)
+        : []
 
 const getNewestVideos = (data) =>
-    data
-        .sort(
-            (videoA, videoB) =>
-                new Date(videoB.publishedAt) - new Date(videoA.publishedAt)
-        )
-        .slice(0, 5)
+    Array.isArray(data) && data
+        ? data
+              .sort(
+                  (videoA, videoB) =>
+                      new Date(videoB.publishedAt) -
+                      new Date(videoA.publishedAt)
+              )
+              .slice(0, 5)
+        : []
 
 const getTotalLikes = (data) =>
-    data.reduce((total, video) => total + parseInt(video.likeCount), 0)
+    Array.isArray(data) && data
+        ? data.reduce((total, video) => total + parseInt(video.likeCount), 0)
+        : 0
 
 const getTopViewedVideos = (data) =>
-    data
-        .sort((videoA, videoB) => videoB.viewCount - videoA.viewCount)
-        .slice(0, 5)
+    Array.isArray(data) && data
+        ? data
+              .sort((videoA, videoB) => videoB.viewCount - videoA.viewCount)
+              .slice(0, 5)
+        : []
 
 export { getTopLikedVideos, getNewestVideos, getTotalLikes, getTopViewedVideos }
